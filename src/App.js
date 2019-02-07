b13dc7cd2f7e54f820ac138e10b5b9eee3593997
MarietaKocharyan <98mairkoch@gmail.com>
2019-02-07 22:26:06 +0400

    Add React-Roter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
-import NestedGrid from './Components/Grid'
+import CenteredGrid from './components/Grid'
+import {
+  BrowserRouter as Router,
+  Switch,
+} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <NestedGrid />
-        </header>
+          <Router >
+            <Switch>
+                {/* <Route exact component={} /> */}
+                {/* <Route path="library" component={MediaGalleryPage} /> */}
+            </Switch>
+          </Router>
+          </header>
       </div>
     );
   }
